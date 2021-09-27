1c7dcd1b93accc14e696dd80d9c2d4a6f2971596
wfahnestock <wfahnestock@student.fullsail.edu>
2021-09-27 19:34:30 -0400

    Complete channel & user search functionality

--- reactjs/client/src/components/ChannelListContainer.js
+++ reactjs/client/src/components/ChannelListContainer.js
@@ -58,7 +58,7 @@ export const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, s
             <SideBar logout={logout} />
             <div className="channel-list__list__wrapper">
                 <CompanyHeader />
-                <ChannelSearch />
+                <ChannelSearch setToggleContainer={setToggleContainer} />
                 <ChannelList
                     filters={filters}
                     channelRenderFilterFn={channelTeamFilter}
