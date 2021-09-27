68cc5823589aaecb9dc294d21bddeed142b70343
wfahnestock <wfahnestock@student.fullsail.edu>
2021-09-27 19:48:47 -0400

    Update incorrect input field

--- reactjs/client/src/components/Auth.js
+++ reactjs/client/src/components/Auth.js
@@ -91,7 +91,7 @@ const Auth = () => {
                                 <label htmlFor="confirmPassword">Confirm Password</label>
                                 <input 
                                     name="confirmPassword"
-                                    type="text"
+                                    type="password"
                                     placeholder="Confirm Password"
                                     onChange={handleChange}
                                     required
